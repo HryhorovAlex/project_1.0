76e0a750152b7b37b8fc1d90d790da0267e72a99
Alexandr <alexandrhryhorov@yojji.io>
2020-09-21 09:39:26 +0300

    add andpoint for todos

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # project_1.0
-Pure js on bakcend and frontend
+Pure js on backend and frontend
--- beckend/server.js
+++ beckend/server.js
@@ -8,30 +8,52 @@ const getRequestRouter = require('./Request-handlers/get-request-handlers')
 
 // server creating
 const http = require('http');
-const postName = '192.168.0.107';
+// const postName = '192.168.0.107'; // Home IP
+const postName = '192.168.1.193' // Yojji IP
 const port = 4001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   // res.end('Hello, who are you? You send plain gat request')
-  const rootHtmlPath = path.join(__dirname, '../frontend/Home-page/index.html');
-  const rootCssPath = path.join(__dirname, '../frontend/Home-page/index.css');
-  const rootJSPath = path.join(__dirname, '../frontend/Home-page/index.js');
-
+  
   switch (req.url) {
     case '/' : {
+      const rootHtmlPath = path.join(__dirname, '../frontend/Home-page/index.html');
       const fileHtml = new fs.ReadStream(rootHtmlPath)
       res.setHeader('Content-Type', 'text/html')
       sendFile(fileHtml, res);
       break;
     }
     case '/index.css' : {
+      const rootCssPath = path.join(__dirname, '../frontend/Home-page/index.css');
       const fileCSS = new fs.ReadStream(rootCssPath)
       res.setHeader('Content-Type', 'text/css')
       sendFile(fileCSS, res);
       break;
     }
     case '/index.js' : {
+      const rootJSPath = path.join(__dirname, '../frontend/Home-page/index.js');
       const fileJS = new fs.ReadStream(rootJSPath)
+      res.setHeader('Content-Type', 'application/javascript')
+      sendFile(fileJS, res);
+      break;
+    }
+    case '/todo' : {
+      const todoHtmlPath = path.join(__dirname, '../frontend/Todo-page/index.html');
+      const fileHtml = new fs.ReadStream(todoHtmlPath)
+      res.setHeader('Content-Type', 'text/html')
+      sendFile(fileHtml, res);
+      break;
+    }
+    case '/index.css' : {
+      const todoCssPath = path.join(__dirname, '../frontend/Todo-page/index.css');
+      const fileCSS = new fs.ReadStream(todoCssPath)
+      res.setHeader('Content-Type', 'text/css')
+      sendFile(fileCSS, res);
+      break;
+    }
+    case '/index.js' : {
+      const todoJSPath = path.join(__dirname, '../frontend/Todo-page/index.js');
+      const fileJS = new fs.ReadStream(todoJSPath)
       res.setHeader('Content-Type', 'application/javascript')
       sendFile(fileJS, res);
       break;
@@ -73,3 +95,4 @@ server.listen(port, postName, () => {
   console.log(`server running at http://${postName}:${port}`)
 })
 
+exports.module = postName;
